3056c9033513e2f1fc320eb239d18a73c17d06a9
Alex Collin <alex@collin.su>
2020-05-12 14:15:08 +0300

    Live comments

--- lib/pxblog_web/channels/user_socket.ex
+++ lib/pxblog_web/channels/user_socket.ex
@@ -2,7 +2,11 @@ defmodule PxblogWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", PxblogWeb.RoomChannel
+  channel "comments:*", PxblogWeb.CommentChannel
+
+  ## Transports
+  # transport :websocket, Phoenix.Transports.WebSocket
+  # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
@@ -15,21 +19,24 @@ defmodule PxblogWeb.UserSocket do
   #
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
-  @impl true
-  def connect(_params, socket, _connect_info) do
-    {:ok, socket}
+  def connect(%{"token" => token}, socket) do
+    case Phoenix.Token.verify(socket, "user", token, max_age: 1209600) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user, user_id)}
+      {:error, reason} ->
+        {:ok, socket}
+    end
   end
 
   # Socket id's are topics that allow you to identify all sockets for a given user:
   #
-  #     def id(socket), do: "user_socket:#{socket.assigns.user_id}"
+  #     def id(socket), do: "users_socket:#{socket.assigns.user_id}"
   #
   # Would allow you to broadcast a "disconnect" event and terminate
   # all active sockets and channels for a given user:
   #
-  #     PxblogWeb.Endpoint.broadcast("user_socket:#{user.id}", "disconnect", %{})
+  #     Pxblog.Endpoint.broadcast("users_socket:#{user.id}", "disconnect", %{})
   #
   # Returning `nil` makes this socket anonymous.
-  @impl true
   def id(_socket), do: nil
 end
